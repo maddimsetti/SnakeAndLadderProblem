dbd5bf9f4efbd1dffb514188610ab52e19068b5a
prudhvi <prudhvieethakota123@gmail.com>
2021-05-02 13:16:24 +0530

    Added:The Player Checks For Option to Move Number of Positions Received in Die

--- SnakeAndLadderProgram.java
+++ SnakeAndLadderProgram.java
@@ -2,13 +2,36 @@ package snakeandladder;
 
 public class SnakeAndLadderProgram {
 
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Welcome the Player to Starts The Snake And Ladder Game at position 0");
 
+		int position = 0;
 		int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
 		System.out.println("The Value Of Dice is " +diceNumber);
+
+		int dieCheck = (int) Math.floor(Math.random() * 100) % 3;
+
+		    switch (dieCheck) {
+		        case LADDER:
+		            System.out.println("The player gets Ladder");
+		            position += diceNumber;
+		            break;
+	                case SNAKE:
+			    System.out.println("the player gets Snake");
+			    position -= diceNumber;
+			         if (position <= 0)
+			             position = 0;
+		            break;
+		        default:
+			    System.out.println("The player gets No Play");
+		            break;
+		    }
+		System.out.println("The Position of Player:" +position);
 
 	}
 
